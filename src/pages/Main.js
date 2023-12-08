e45dd933d5613a2f1cf70bbb9c49e97a1ab999be
bitnaleeeee <bitnaleeeee@gmail.com>
2023-12-08 20:51:31 +0900

    컴포넌트구조변경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import App from "../Lexical/App";
 import Notebooks from "../Compotnets/Notebooks";
-import Memos from "../Compotnets/Memos";
 import "./Main.scss";
 
 const Main = () => {
+  const [memo, setMemo] = useState([]);
+
+  function addNewMemo() {
+    let inputText = JSON.parse(localStorage.getItem("inputText"));
+    setMemo([...memo, inputText]);
+  }
+
   return (
     <div className="upnoteWrap">
       <div className="upnoteIneer">
@@ -15,8 +21,15 @@ const Main = () => {
         </div>
         <div className="memoWrap">
           변수들어갈거임
-          <Memos />
+          <ul>
+            {memo.map((item, idx) =>
+              item.length ? <li key={idx}>{item} </li> : null
+            )}
+          </ul>
         </div>
+        <button className="memoAdd" type="submit" onClick={addNewMemo}>
+          새 노트
+        </button>
         <div className="editorWrap">
           에디터
           <App />
